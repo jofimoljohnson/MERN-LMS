a345d311dba4d154ad089ae941d2a8c49942260b
jofimoljohnson <jofimolj@gmail.com>
2025-07-23 10:31:57 +0530

    last commit

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://mern-lms-lovat.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
