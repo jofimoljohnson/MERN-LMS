45f0d9af8cee7bcc986bc15c8b0a9c43496a37fe
jofimoljohnson <jofimolj@gmail.com>
2025-07-23 10:27:08 +0530

    final commit

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000"
+    // baseURL:"http://localhost:5000"
+        baseURL:"https://mern-lms-zmng.onrender.com"
+
 })
 
 axiosInstance.interceptors.request.use(config=>{
